dee7cfa89f9121cdf240344cd813ce1cb0a90ff2
Daniel <dannyblaze469@gmail.com>
2021-04-02 15:35:13 -0400

    Added validation to cat model

--- cats_99/app/models/cat.rb
+++ cats_99/app/models/cat.rb
@@ -16,6 +16,23 @@ require 'action_view'
 
 class Cat < ApplicationRecord
     include ActionView::Helpers::DateHelper
+    
+    COLORS = [
+        "White",
+        "Black",
+        "Orange",
+        "Gray",
+        "Purple"
+    ]
+
+    SEX = ["M", "F"]
+
+    validates :birth_date, :name, :color, :sex, presence: true
+    validates :color, inclusion: { in: COLORS,
+    message: "Invalid Color" }
+
+    validates :sex, inclusion: { in: SEX,
+    message: "Invalid sex" }
 
     def age
         # 2021/4/2 - cat's bday = age
