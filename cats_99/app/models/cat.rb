f1d0723196681d1b8387c3d28448b8f49ca8085e
Daniel <dannyblaze469@gmail.com>
2021-04-02 14:46:24 -0400

    Made first Migration

---
+++ cats_99/app/models/cat.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+
+    def age
+       :birth_date 
+    end
+
+end
